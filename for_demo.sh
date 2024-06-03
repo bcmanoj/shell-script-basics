13682ec395c281929aac0b734835ae8c27973a5b
vboxuser <vboxuser@Ubuntu-18.myguest.virtualbox.org>
2024-06-03 08:50:26 +0530

    Added scripts to demo for loop

---
+++ for_demo.sh
@@ -0,0 +1,10 @@
+
+# Simple for demo bash shell script.
+
+echo "Shows Given List of fruits: "
+echo 
+for item in mango banana chikku grapes
+do
+	echo $item
+
+done
---
+++ for_sec_demo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Name : for_sec_demo.sh
+# Description : Loops over list of strings
+# Author : Omkar MN
+
+echo "**** List of host names in HCS Engineering Team are as follows ***** :"
+for manu in system-linux-01 system-linux-02 system-linux-03 system-linux-04
+do
+       echo $manu	
+
+done
